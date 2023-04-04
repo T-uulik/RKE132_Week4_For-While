f210688c39acd47ac635b6f3d31920f1d74623ce
T-uulik <tonis.tuulik@tktk.ee>
2023-04-04 21:06:26 +0300

    first commit

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rng  = new Random();
+
+int myRandomNum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    myRandomNum = rng.Next(0, 11);
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is :{myRandomNum}");
+}
+
+Console.WriteLine($"Random Num Sum total: {randomSum}");
+
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.ComponentModel.Design;
+
+Random rng = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+
+while (loopActive)
+{
+    cpuRandom = rng.Next(1, 4);
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+    int userNumber = Int32.Parse(Console.ReadLine());
+    //int userCheat = 99;
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratz, you won!");
+        loopActive = false;
+    }
+    /*else if (userCheat == userNumber)
+    {
+        Console.WriteLine($"cpu has generated {cpuRandom}"); proovisin cheat code sisse kirjutada, kuid ei närinud läbi, et kuidas sama loopi hoida peale cheat code kasutamist.
+        Console.WriteLine("Cheater >:("); 
+    }*/
+    else
+    {
+        Console.WriteLine("Oops. Try again.");
+    }
+   
+
+}
+Console.WriteLine("Bye Bye!");
+
+
+
